713434dfb62e46392c0bf247220307ad11c724a0
Ben <ben.rutter@thameswater.co.uk>
2021-12-30 14:05:19 +0000

    first pass working model

--- cnn.py
+++ cnn.py
@@ -5,40 +5,38 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping
 
 
-def train_and_test_iterators():
-    train_generator = ImageDataGenerator(rescale=1.0/255, horizontal_flip=True)
+def train_and_test_iterators(batch_size):
+    train_generator = ImageDataGenerator(rescale=1.0/255, horizontal_flip=True, zoom_range=0.1)
     test_generator = ImageDataGenerator(rescale=1.0/255)
     train_iterator = train_generator.flow_from_directory(
-        'train',
-        target_size=(256, 256),
+        'data/train',
+        target_size=(120, 120),
         batch_size=batch_size,
-        color_mode='grayscale',
-        class_mode='categorical',
+        class_mode='binary',
     )
     test_iterator = test_generator.flow_from_directory(
-        'test',
-        target_size=(256, 256),
+        'data/test',
+        target_size=(120, 120),
         batch_size=batch_size,
-        color_mode='grayscale',
-        class_mode='categorical',
+        class_mode='binary',
     )
     return train_iterator, test_iterator
 
 
 def build_model():
     model = Sequential()
-    model.add(keras.Input(shape=(256, 256, 1)))
-    model.add(layers.Conv2D(15, 5, strides=2, activation='relu'))
+    model.add(keras.Input(shape=(120, 120, 3)))
+    model.add(layers.Conv2D(32, (3, 3), strides=2, activation='relu'))
     model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(layers.Conv2D(15, 3, strides=2, activation='relu'))
+    model.add(layers.Conv2D(32, (3, 3), strides=2, activation='relu'))
     model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(layers.Flatten())
-    model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dense(3, activation='softmax'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
     model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=0.001),
-        loss=keras.losses.CategoricalCrossentropy(),
-        metrics=[keras.metrics.CategoricalAccuracy(), keras.metrics.AUC()],
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=['accuracy'],
     )
     return model
 
@@ -49,15 +47,21 @@ if __name__ == '__main__':
     epochs = 25
 
     early_stop = EarlyStopping(
-        monitor='val_auc', mode='min', verbose=1, patience=20)
-    train_iterator, test_iterator = train_and_test_iterators()
+        monitor='val_loss',
+        mode='min',
+        patience=5,
+    )
+
+    train_iterator, test_iterator = train_and_test_iterators(batch_size)
     model = build_model()
 
     model.fit(
         train_iterator,
-        steps_per_epoch=train_iterator.samples/batch_size,
+        steps_per_epoch=int(train_iterator.samples/batch_size),
         epochs=epochs,
         validation_data=test_iterator,
-        validation_steps=test_iterator.samples/batch_size,
+        validation_steps=int(test_iterator.samples/batch_size),
         callbacks=[early_stop],
     )
+
+    model.save_model('cnn')
---
+++ model_predictions.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+import pandas as pd
+
+label_generator = ImageDataGenerator(rescale=1.0/255)
+label_iterator = label_generator.flow_from_directory(
+    'data/to_predict',
+    target_size=(120, 120),
+    batch_size=32,
+    shuffle=False,
+    class_mode=None,
+)
+
+model = load_model('cnn', compile=True)
+
+labels = model.predict(label_iterator)
+
+subm = pd.read_csv('sample_submission.csv')
+ids = [int(x.split("\\")[1].split(".")[0]) for x in label_iterator.filenames]
+
+for i in range(len(ids)):
+    subm.loc[subm.id == ids[i], "label"] = labels[:, 0][i]
+
+subm.to_csv('submission.csv', index=False)
